2cceac44fef94461eda4e479ccbf5bd3febc5cd7
Yago Ananias <yago_souza92@yahoo.com.br>
2021-07-10 11:47:25 -0300

    Aplicando filtros

--- src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -15,7 +15,11 @@ public class RotaPedidos {
 			public void configure() throws Exception {
 				
 				from("file:pedidos?delay=5s&noop=true").
-					log("${id}").
+					split().
+						xpath("/pedido/itens/item").
+						log("${body}").
+					filter().
+						xpath("/item/formato[text()='EBOOK']").
 					marshal().xmljson().
 					log("${body}").
 					setHeader("CamelFileName", simple("${file:name.noext}.json")).
